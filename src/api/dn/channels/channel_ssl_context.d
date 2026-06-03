170e9af72326711486995cc7c02489e9a4a66394
initkfs <user@user>
2026-06-03 19:19:01 +0300

    Extract chan ssl context

---
+++ src/api/dn/channels/channel_ssl_context.d
@@ -0,0 +1,84 @@
+module api.dn.channels.channel_ssl_context;
+
+import openssl_libs : SSL, BIO, SSL_shutdown, SSL_free, BIO_reset, SSL_clear;
+
+/**
+ * Authors: initkfs
+ */
+struct ChannelSSLContext
+{
+    SSL* ssl;
+    BIO* rbio;
+    BIO* wbio;
+    bool isInitSSL;
+    bool isOpen;
+
+    void clear()
+    {
+        ssl = null;
+        rbio = null;
+        wbio = null;
+        isInitSSL = false;
+        isOpen = false;
+    }
+
+    void shutdown()
+    {
+        if (ssl)
+        {
+            SSL_shutdown(ssl);
+        }
+    }
+
+    void close()
+    {
+        if (!isOpen && !isInitSSL)
+        {
+            return;
+        }
+
+        if (ssl)
+        {
+            SSL_clear(ssl);
+        }
+
+        resetbio;
+        isOpen = false;
+        isInitSSL = false;
+    }
+
+    void resetbio()
+    {
+        if (rbio)
+        {
+            BIO_reset(rbio);
+        }
+
+        if (wbio)
+        {
+            BIO_reset(wbio);
+        }
+    }
+
+    void freeSSL()
+    {
+        if (ssl)
+        {
+            SSL_free(ssl);
+            ssl = null;
+        }
+    }
+
+    void reset()
+    {
+        if (ssl)
+        {
+            //TODO errror code == -1
+            SSL_clear(ssl);
+        }
+
+        resetbio;
+        isOpen = false;
+        isInitSSL = false;
+    }
+}
--- src/api/dn/channels/fd_channel.d
+++ src/api/dn/channels/fd_channel.d
@@ -2,6 +2,7 @@ module api.dn.channels.fd_channel;
 
 import api.dn.sockets.socket_connect : SocketConnectState;
 import api.dn.channels.fd_channel_buffers : InBuffer, OutBuffer;
+import api.dn.channels.channel_ssl_context: ChannelSSLContext;
 
 /**
  * Authors: initkfs
@@ -12,88 +13,6 @@ enum FdChannelType
     socket,
     timer,
     file
-}
-
-struct SSLContext
-{
-    import openssl_libs : SSL, BIO, SSL_shutdown, SSL_free, BIO_reset, SSL_clear;
-
-    SSL* ssl;
-    BIO* rbio;
-    BIO* wbio;
-    bool isInitSSL;
-    bool isOpen;
-
-    void clear()
-    {
-        ssl = null;
-        rbio = null;
-        wbio = null;
-        isInitSSL = false;
-        isOpen = false;
-    }
-
-    void shutdown()
-    {
-        if (ssl)
-        {
-            SSL_shutdown(ssl);
-        }
-    }
-
-    void close()
-    {
-        if (!isOpen && !isInitSSL)
-        {
-            return;
-        }
-
-        if (ssl)
-        {
-            SSL_clear(ssl);
-        }
-
-        resetbio;
-        isOpen = false;
-        isInitSSL = false;
-    }
-
-    void resetbio()
-    {
-        if (rbio)
-        {
-            BIO_reset(rbio);
-        }
-
-        if (wbio)
-        {
-            BIO_reset(wbio);
-        }
-    }
-
-    void freeSSL()
-    {
-        if (ssl)
-        {
-            SSL_free(ssl);
-            ssl = null;
-        }
-    }
-
-    void reset()
-    {
-        if (ssl)
-        {
-            //TODO errror code == -1
-            SSL_clear(ssl);
-        }
-
-        resetbio;
-        isOpen = false;
-        isInitSSL = false;
-
-
-    }
 }
 
 struct FdChannel
@@ -107,7 +26,7 @@ struct FdChannel
     OutBuffer outb;
     bool isChain;
 
-    SSLContext sslContext;
+    ChannelSSLContext sslContext;
 
     void* data;
 
@@ -120,7 +39,7 @@ struct FdChannel
         outb.reset;
         inb.reset;
 
-        sslContext = SSLContext();
+        sslContext = ChannelSSLContext();
         resetPart;
     }
 
